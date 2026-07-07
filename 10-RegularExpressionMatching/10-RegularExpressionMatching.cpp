c7861a30613147101f8961f45c1e3954cb85c03d
Naman prasad <namanprasad281@gmail.com>
2026-07-07 15:51:30 +0530

    File updated at 7/7/2026, 3:51:27 PM

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.cpp
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.cpp
@@ -1,33 +1,39 @@
-// Last updated: 6/19/2026, 3:29:30 AM
+// Last updated: 7/7/2026, 3:51:27 PM
 1class Solution {
-2public:  
-3  map<int,vector<char>> mpp;
-4  vector<string> ans;
-5      void func(int i , string &digits,string s){
-6       if(i>=digits.size()){
-7            ans.push_back(s);
-8            return ;
-9       }
-10        
-11        for(auto it:mpp[digits[i]]){
-12          
-13            string t = s + it;
-14          
-15                 func(i+1,digits,t);
-16        }
-17      }
-18    vector<string> letterCombinations(string digits) {
-19         
-20         mpp['2'] ={'a','b','c'};
-21         mpp['3'] ={'d','e','f'};
-22         mpp['4'] ={'g','h','i'};
-23         mpp['5'] ={'j','k','l'};
-24         mpp['6'] ={'m','n','o'};
-25         mpp['7'] ={'p','q','r','s'};
-26         mpp['8'] ={'t','u','v'};
-27         mpp['9'] ={'w','x','y','z'};
-28
-29     func(0,digits,"");
-30return ans;
-31    }
-32};
+2public:
+3    string s, p;
+4    vector<vector<int>> dp;
+5
+6    bool func(int i, int j) {
+7
+8        if (j == p.size())
+9            return i == s.size();
+10
+11        if (dp[i][j] != -1)
+12            return dp[i][j];
+13
+14        bool firstMatch =
+15            (i < s.size()) &&
+16            (s[i] == p[j] || p[j] == '.');
+17
+18        bool ans;
+19
+20        if (j + 1 < p.size() && p[j + 1] == '*') {
+21            ans = func(i, j + 2) ||
+22                  (firstMatch && func(i + 1, j));
+23        } else {
+24            ans = firstMatch && func(i + 1, j + 1);
+25        }
+26
+27        return dp[i][j] = ans;
+28    }
+29
+30    bool isMatch(string str, string pat) {
+31        s = str;
+32        p = pat;
+33
+34        dp.assign(s.size() + 1, vector<int>(p.size() + 1, -1));
+35
+36        return func(0, 0);
+37    }
+38};
