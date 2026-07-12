6c58c4ef797cd47505c9b5854b1e607b7a9ab4c1
Naman prasad <namanprasad281@gmail.com>
2026-07-12 14:29:16 +0530

    File updated at 7/12/2026, 2:29:12 PM

--- 1331-RankTransformofanArray/1331-RankTransformofanArray.cpp
+++ 1331-RankTransformofanArray/1331-RankTransformofanArray.cpp
@@ -1,17 +1,37 @@
-// Last updated: 7/12/2026, 1:12:26 PM
+// Last updated: 7/12/2026, 2:29:12 PM
 1class Solution {
 2public:
-3    bool judgeCircle(string moves) {
-4        if (moves.length() & 1) return false;
-5        int x = 0, y = 0;
-6
-7        for (char c : moves) {
-8            y += (c == 'U') - (c == 'D');
-9            x += (c == 'R') - (c == 'L');
-10        }
-11
-12        return !x && !y;
-13    }
-14};
+3    int maxRotateFunction(vector<int>& nums) {
+4        
+5
+6        long long x = accumulate( nums.begin() , nums.end(),0);
+7
+8        vector<long long> pref(nums.size(),0);
+9
+10        for(int i=0;i<nums.size(); i++){
+11            pref[i] =  x -  nums[i];
+12            
+13        }
+14        long long curr = 0;
 15
-16
+16        for(int i=0;i<nums.size();i++){
+17         curr += nums[i]*i;
+18
+19        }
+20
+21        long long ans = 0;
+22        ans = curr;
+23
+24
+25
+26        for(int i=0;i<nums.size();i++){
+27        long long tmp = curr - pref[i];
+28        tmp += nums[i]*(nums.size()-1);
+29    
+30        curr = tmp;
+31    
+32        ans = max(ans,tmp);
+33        }
+34        return ans;
+35    }
+36};
