5e42d7bba39804bdae7afc81d6f659e5bc10ae56
Naman prasad <namanprasad281@gmail.com>
2026-07-15 16:34:38 +0530

    File updated at 7/15/2026, 4:34:35 PM

--- 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.cpp
+++ 3658-GCDofOddandEvenSums/3658-GCDofOddandEvenSums.cpp
@@ -1,10 +1,7 @@
-// Last updated: 7/15/2026, 4:33:25 PM
+// Last updated: 7/15/2026, 4:34:35 PM
 1class Solution {
 2public:
 3    int gcdOfOddEvenSums(int n) {
-4
-5
-6     
-7        return n;
-8    }
-9};
+4        return n;
+5    }
+6};
