18d06d2d9b61a146735ee8034e2a490344384532
Naman prasad <namanprasad281@gmail.com>
2026-07-16 00:27:13 +0530

    File updated at 7/16/2026, 12:27:10 AM

--- 3193-CounttheNumberofInversions/3193-CounttheNumberofInversions.cpp
+++ 3193-CounttheNumberofInversions/3193-CounttheNumberofInversions.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/16/2026, 12:26:12 AM
+// Last updated: 7/16/2026, 12:27:10 AM
 1class Solution {
 2public:
 3   int n;
@@ -8,37 +8,35 @@
 7    int func(int i, int inv){
 8      if (inv > 400)
 9        return 0;
-10    if(inv > (i+1)*i/2)
-11        return 0;
-12
-13    if(mpp.count(i) && inv != mpp[i])
-14        return 0;
-15
-16    if(i == n-1)
-17        return 1;
-18
-19    if(dp[i][inv] != -1)
-20        return dp[i][inv];
+10   
+11    if(mpp.count(i) && inv != mpp[i])
+12        return 0;
+13
+14    if(i == n-1)
+15        return 1;
+16
+17    if(dp[i][inv] != -1)
+18        return dp[i][inv];
+19
+20    long long ans = 0;
 21
-22    long long ans = 0;
+22    for(int add = 0; add <= i+1; add++){
 23
-24    for(int add = 0; add <= i+1; add++){
-25
-26        ans += func(i+1, inv + add);
-27        ans %= mod;
-28    }
-29
-30    return dp[i][inv] = ans;
-31}
-32    int numberOfPermutations(int n, vector<vector<int>>& requirements) {
-33        map<int,int> mpp;
-34
-35        for(auto &it : requirements){
-36            mpp[it[0]] = it[1];
-37                    }
-38             this->mpp= mpp;
-39             this->n=n;
-40           dp.assign(n+1,vector<int> (401,-1));
-41          return func(0,0);
-42    }
-43};
+24        ans += func(i+1, inv + add);
+25        ans %= mod;
+26    }
+27
+28    return dp[i][inv] = ans;
+29}
+30    int numberOfPermutations(int n, vector<vector<int>>& requirements) {
+31        map<int,int> mpp;
+32
+33        for(auto &it : requirements){
+34            mpp[it[0]] = it[1];
+35                    }
+36             this->mpp= mpp;
+37             this->n=n;
+38           dp.assign(n+1,vector<int> (401,-1));
+39          return func(0,0);
+40    }
+41};
