cbeb61e84db3812a0767b70bedcae8f4261d8729
Naman prasad <namanprasad281@gmail.com>
2026-07-26 01:02:19 +0530

    File updated at 7/26/2026, 1:02:17 AM

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.cpp
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/26/2026, 1:01:48 AM
+// Last updated: 7/26/2026, 1:02:17 AM
 1class MedianFinder {
 2public:
 3   priority_queue<int,vector<int> , greater<int>> pq1;
@@ -16,40 +16,43 @@
 15            pq2.push(num);
 16        }
 17        
-18        else if(!pq1.empty() and pq1.top()  <= num){
+18       else if(!pq1.empty() and pq1.top()  <= num){
 19             pq1.push(num);
 20        }
-21        
-22
-23        if(abs((int)pq1.size() - (int)pq2.size()) > 1 ) {
-24
-25            if(pq1.size() > pq2.size()){
-26                pq2.push(pq1.top());
-27                pq1.pop();
-28            }
-29            else{
-30                pq1.push(pq2.top());
-31                pq2.pop();
-32            }
-33        }
-34       
-35    }
-36    
-37    double findMedian() {
-38            if (pq1.size() == pq2.size()) {
-39            // Use double casting to prevent integer overflow during addition
-40            return ((double)pq1.top() + (double)pq2.top()) / 2.0;
-41        }
-42        if(pq1.size()> pq2.size()) return pq1.top();
-43
-44        return pq2.top();
-45        
-46    }
-47};
-48
-49/**
-50 * Your MedianFinder object will be instantiated and called as such:
-51 * MedianFinder* obj = new MedianFinder();
-52 * obj->addNum(num);
-53 * double param_2 = obj->findMedian();
-54 */
+21
+22        if(abs((int)pq1.size() - (int)pq2.size()) > 1 ) {
+23
+24            if(pq1.size() > pq2.size()){
+25                pq2.push(pq1.top());
+26                pq1.pop();
+27            }
+28            else{
+29                pq1.push(pq2.top());
+30                pq2.pop();
+31            }
+32        }
+33       
+34    }
+35    
+36    double findMedian() {
+37        if(pq2.size()==pq1.size()){
+38              
+39              int t =  pq2.top();
+40              int t2 = pq1.top();
+41              double x = (t+t2);
+42              x/=2;
+43              return x;
+44        }
+45        if(pq1.size()> pq2.size()) return pq1.top();
+46
+47        return pq2.top();
+48        
+49    }
+50};
+51
+52/**
+53 * Your MedianFinder object will be instantiated and called as such:
+54 * MedianFinder* obj = new MedianFinder();
+55 * obj->addNum(num);
+56 * double param_2 = obj->findMedian();
+57 */
