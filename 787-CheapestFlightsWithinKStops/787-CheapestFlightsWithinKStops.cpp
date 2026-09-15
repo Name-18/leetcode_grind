949e08e0ab94493ec50b12767cbbe2c9407df368
Naman prasad <namanprasad281@gmail.com>
2026-09-16 00:09:15 +0530

    File updated at 9/16/2026, 12:09:12 AM

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.cpp
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.cpp
@@ -1,39 +1,53 @@
-// Last updated: 8/5/2026, 6:31:37 PM
+// Last updated: 9/16/2026, 12:09:12 AM
 1class Solution {
 2public:
-3    using tp = tuple<int, int, int>;
-4    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-5                          int k) {
-6        vector<vector<pair<int, int>>> adj(n);
-7        for (auto it : flights) {
-8            int fr = it[0];
-9            int to = it[1];
-10            int pr = it[2];
-11            adj[fr].push_back({to, pr});
-12        }
-13        vector<vector<int>> dist(n, vector<int>(k + 2, 1e9));
-14        priority_queue<tp, vector<tp>, greater<tp>> pq;
-15        pq.push({0, 0, src});
-16        dist[src][0] = 0;
-17        while (!pq.empty()) {
-18            auto x = pq.top();
-19            pq.pop();
-20            int pr = std::get<0>(x);
-21            int stp = std::get<1>(x);
-22            int nd = std::get<2>(x);
-23            if (stp >= k+1 )
-24                continue;
-25            for (auto it : adj[nd]) {
-26
-27                int nxt = it.first;
-28                int p = it.second;
-29
-30                if (dist[nxt][stp + 1] > pr + p) {
-31                    dist[nxt][stp + 1] = pr + p;
-32                    pq.push({pr + p, stp + 1, nxt});
-33                }
-34            }
-35        }
-36        return *min_element(dist[dst].begin(), dist[dst].end()) == 1e9 ? -1:*min_element(dist[dst].begin(), dist[dst].end()) ;
-37    }
-38};
+3    int inf = (int)1e9;
+4    using t = pair<int, pair<int, int>>;
+5    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+6                          int k) {
+7
+8        vector<vector<int>> dis(n, vector<int>(k + 2, inf));
+9
+10        vector<vector<pair<int, int>>> adj(n);
+11
+12        for (auto& it : flights) {
+13            int fr = it[0];
+14            int to = it[1];
+15            int wt = it[2];
+16
+17            adj[fr].push_back({to, wt});
+18        }
+19
+20        priority_queue<t, vector<t>, greater<t>> pq;
+21        dis[src][0]=0;
+22        pq.push({0, {0, src}});
+23
+24        while (!pq.empty()) {
+25
+26            auto it = pq.top();
+27            pq.pop();
+28
+29            int nd = it.second.second;
+30            int wt = it.first;
+31            int stp = it.second.first;
+32        if(dis[nd][stp] < wt) continue;
+33            for (auto it : adj[nd]) {
+34
+35                int nw = wt + it.second;
+36                int node = it.first;
+37
+38                if (stp+ 1 <= k+1 and dis[node][stp+ 1] > nw) {
+39                    dis[node][stp+ 1] = nw;
+40
+41                    pq.push({nw, {stp + 1, node}});
+42                }
+43            }
+44        }
+45
+46int ans = inf;
+47        for(int i=0;i<k+2;i++){
+48          ans = min(ans , dis[dst][i]);
+49        }
+50         return ans == inf ? -1 : ans;
+51    }
+52};
